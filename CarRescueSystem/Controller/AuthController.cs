d56377a13e4eaf544ebff5e7c379604dca77f66a
Hai-Dang-123 <danglvhse151369@fpt.edu.vn>
2025-02-13 14:29:48 +0700

    Re-add CarRescueSystem as a normal directory

---
+++ CarRescueSystem/Controller/AuthController.cs
@@ -0,0 +1,71 @@
+﻿using CarRescueSystem.BLL.Service.Interface;
+using CarRescueSystem.Common.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRescueSystem.Controller
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        /// <summary>
+        /// Đăng nhập người dùng
+        /// </summary>
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO loginDTO)
+        {
+            var response = await _authService.Login(loginDTO);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        /// <summary>
+        /// Đăng ký người dùng
+        /// </summary>
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDTO registerDTO)
+        {
+            var response = await _authService.Register(registerDTO);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        /// <summary>
+        /// Làm mới cả AccessToken và RefreshToken
+        /// </summary>
+        [HttpPost("refresh")]
+        public async Task<IActionResult> Refresh([FromBody] TokenDTO tokenDTO)
+        {
+            // Kiểm tra thông tin token
+            if (tokenDTO == null || string.IsNullOrWhiteSpace(tokenDTO.RefreshToken) || string.IsNullOrWhiteSpace(tokenDTO.AccessToken))
+            {
+                return BadRequest(new ResponseDTO("Access token and refresh token are required.", 400, false)); 
+            }
+
+            var response = await _authService.RefreshBothTokens(tokenDTO.AccessToken, tokenDTO.RefreshToken);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response); 
+            }
+
+       
+            return StatusCode(response.StatusCode, response);
+        }
+
+        /// <summary>
+        /// Đăng xuất và thu hồi RefreshToken
+        /// </summary>
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout([FromBody] string refreshToken)
+        {
+            var response = await _authService.LogoutAsync(refreshToken);
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
---
+++ CarRescueSystem/Program.cs
@@ -0,0 +1,60 @@
+﻿using CarRescueSystem.BLL.Service.Implement;
+using CarRescueSystem.BLL.Service.Interface;
+using CarRescueSystem.BLL.Utilities;
+using CarRescueSystem.DAL;
+using CarRescueSystem.DAL.Data;
+using CarRescueSystem.DAL.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Thêm CORS vào services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000") // URL React chạy trên cổng 3000
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+// Setup SQL Server Database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Swagger
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Add các dịch vụ
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<UserUtility>();
+
+// Đăng ký DbSeeder vào DI container
+builder.Services.AddScoped<DbSeeder>();
+
+// Thêm IHttpContextAccessor
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+var app = builder.Build();
+
+
+// Sử dụng CORS
+app.UseCors("AllowReactApp");
+
+// Bật Swagger
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Car Rescue API V1");
+    c.RoutePrefix = "swagger"; // Truy cập trực tiếp tại http://localhost:5210/
+});
+app.UseRouting();
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
