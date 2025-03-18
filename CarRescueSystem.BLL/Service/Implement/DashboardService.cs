1b15f145e681c55b3e49ef526fd4d9cd7a175921
Vũ Huy Hoàng <hoangvhse161484@fpt.edu.vn>
2025-03-18 23:59:26 +0700

    update dashboard for fe to get

--- CarRescueSystem.BLL/Service/Implement/DashboardService.cs
+++ CarRescueSystem.BLL/Service/Implement/DashboardService.cs
@@ -27,14 +27,13 @@ namespace CarRescueSystem.BLL.Service.Implement
             var serviceCount = await _context.Services.CountAsync();
             var bookingCount = await _context.Bookings.CountAsync();
             var monthlyRevenue = await _context.Bookings
-                .Where(b => b.bookingDate.Month == DateTime.Now.Month && b.bookingDate.Year == DateTime.Now.Year)
+                .Where(b => b.bookingDate.Month == DateTime.Now.Month)
                 .SumAsync(b => b.totalPrice) ?? 0;
 
             var monthlyRevenues = await _context.Bookings
-                .GroupBy(b => new { b.bookingDate.Year, b.bookingDate.Month })
+                .GroupBy(b => new {b.bookingDate.Month})
                 .Select(g => new MonthlyRevenueDto
                 {
-                    Year = g.Key.Year,
                     Month = g.Key.Month,
                     Revenue = g.Sum(b => b.totalPrice) ?? 0
                 })
--- CarRescueSystem.Common/DTO/DashboardDto.cs
+++ CarRescueSystem.Common/DTO/DashboardDto.cs
@@ -18,7 +18,6 @@ namespace CarRescueSystem.Common.DTO
     public class MonthlyRevenueDto
         {
         public int Month { get; set; }
-        public int Year { get; set; }
         public decimal Revenue { get; set; }
         }
 } 
