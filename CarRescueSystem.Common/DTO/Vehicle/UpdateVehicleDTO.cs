60de1d09b86d05163bba7e01eab7bc1e6cca3760
Vũ Huy Hoàng <hoangvhse161484@fpt.edu.vn>
2025-03-27 14:05:07 +0700

    update project

--- CarRescueSystem.Common/DTO/Vehicle/UpdateVehicleDTO.cs
+++ CarRescueSystem.Common/DTO/Vehicle/UpdateVehicleDTO.cs
@@ -3,7 +3,10 @@ namespace CarRescueSystem.Common.DTO.Vehicle
     public class UpdateVehicleDTO
     {
 
+        public string model { get; set; }
         public string color { get; set; }
+        public string brand { get; set; }
+        public int numberOfSeats { get; set; }
 
     }
 } 
